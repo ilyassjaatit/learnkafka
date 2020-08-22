d570c015be0d39fa58a90a17724382699effdef1
ilyass jaatit <ilyassjaatit@gmail.com>
2020-08-22 21:40:20 +0200

    Configuring DRF to generate a token at login

--- backend/account/api/urls.py
+++ backend/account/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from account.api.views import api_detail_user_view, api_register_user_view
 
 app_name = "account"
 
 urlpatterns = [
+    path('login/', views.obtain_auth_token, name="auth_token"),
     path('register/', api_register_user_view, name='register'),
     path('<pk>/', api_detail_user_view, name='detail')
 ]
--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -38,6 +38,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders'
 ]
 
